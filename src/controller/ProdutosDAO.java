e74c8e7bcd70402f65970ab3de11ad4345af50c9
eclipseCJP <cris.tecwordd@gmail.com>
2023-07-09 10:34:25 -0400

    funcionalidade listar produtos implementada

--- src/controller/ProdutosDAO.java
+++ src/controller/ProdutosDAO.java
@@ -4,29 +4,27 @@ package controller;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author Adm
  */
-
 import model.ProdutosDTO;
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.util.Set;
 import javax.swing.JOptionPane;
 
-
 public class ProdutosDAO {
-    
+
     Connection conn;
     PreparedStatement pstm;
-    ResultSet resultset;
+    ResultSet rs;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto){
+
+    public void cadastrarProduto(ProdutosDTO produto) {
         conn = new ConectaDAO().connectDB();
         try {
             pstm = conn.prepareStatement("INSERT INTO produtos (nome,valor,status) VALUES (?, ?, ?)");
@@ -34,19 +32,32 @@ public class ProdutosDAO {
             pstm.setDouble(2, produto.getValor());
             pstm.setString(3, produto.getStatus());
             pstm.executeUpdate();
-            JOptionPane.showMessageDialog(null  , "Produto cadastrado com sucesso!");
+            JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!");
         } catch (SQLException e) {
             e.printStackTrace();
-        } 
+        }
     }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
+
+    public ArrayList<ProdutosDTO> listarProdutos() {
+        conn = new ConectaDAO().connectDB(); 
+
+        try {
+            pstm = conn.prepareStatement("SELECT * FROM produtos");
+            rs = pstm.executeQuery();
+
+            while (rs.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(rs.getInt("id"));
+                produto.setNome(rs.getString("nome"));
+                produto.setValor(rs.getInt("valor"));
+                produto.setStatus(rs.getString("status"));
+                
+                listagem.add(produto);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return listagem;
     }
-    
-    
-    
-        
-}
 
+}
