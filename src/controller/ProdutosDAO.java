6f9e6fc704f9281a809775217e80f75593d3efd4
eclipseCJP <cris.tecwordd@gmail.com>
2023-07-09 09:33:50 -0400

    funcionalidade cadastro implementada

--- src/controller/ProdutosDAO.java
+++ src/controller/ProdutosDAO.java
@@ -13,21 +13,31 @@ package controller;
 import model.ProdutosDTO;
 import java.sql.PreparedStatement;
 import java.sql.Connection;
-import javax.swing.JOptionPane;
+import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 
 public class ProdutosDAO {
     
     Connection conn;
-    PreparedStatement stm;
+    PreparedStatement pstm;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
         conn = new ConectaDAO().connectDB();
-        
+        try {
+            pstm = conn.prepareStatement("INSERT INTO produtos (nome,valor,status) VALUES (?, ?, ?)");
+            pstm.setString(1, produto.getNome());
+            pstm.setDouble(2, produto.getValor());
+            pstm.setString(3, produto.getStatus());
+            pstm.executeUpdate();
+            JOptionPane.showMessageDialog(null  , "Produto cadastrado com sucesso!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } 
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
--- src/controller/conectaDAO.java
+++ src/controller/conectaDAO.java
@@ -3,7 +3,7 @@ package controller;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import javax.swing.JOptionPane;
+
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -37,9 +37,15 @@ public class ConectaDAO {
         }
     }
     
-    public static void main(String[] args) {
-        ConectaDAO conexao = new ConectaDAO();
-        conexao.connectDB();
+    public void disconnect(Connection conn){
+        try{
+            if(conn != null && !conn.isClosed()){
+                conn.close();
+                System.out.println("Você se desconectou do banco de dados");
+            }
+        }catch(SQLException e){
+            System.out.println("Erro:não foi possivel se desconectar do banco de dados..."+e.getMessage());
+        }
     }
 
 }
